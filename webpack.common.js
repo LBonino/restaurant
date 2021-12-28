dda286989e341a69234ae350861c8b5df7440c8a
Lisandro Bonino <lisandrobonino4@protonmail.com>
2021-12-28 11:12:00 -0300

    build: let webpack handle styles, images and fonts

--- webpack.common.js
+++ webpack.common.js
@@ -15,4 +15,20 @@ module.exports = {
             template: "./src/templates/index.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
