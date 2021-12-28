87c86c2ad72e955cb7418b6a86efc20016ce17fc
Lisandro Bonino <lisandrobonino4@protonmail.com>
2021-12-28 11:14:54 -0300

    build: listen for requests from the network

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,8 @@ module.exports = merge(common, {
     devtool: "inline-source-map",
     devServer: {
         static: "./dist",
+        host: "0.0.0.0",
+        port: 8080,
         watchFiles: ["./src/templates/*.html"],
     }
 });
