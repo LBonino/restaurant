a2af80c5bd81bbe960f195c0efda8256682316c5
Lisandro Bonino <lisandrobonino4@protonmail.com>
2022-01-02 14:22:08 -0300

    add: module for contact tab

--- src/static/js/contact.js
+++ src/static/js/contact.js
@@ -1,7 +1,22 @@
 const tabHeading = "Contact us";
 
 const tabContent = (() => {
-    // TODO
+    const contact = document.createElement("div");
+    contact.id = "contact-tab";
+
+    const phone = document.createElement("p");
+    const email = document.createElement("p");
+    const address = document.createElement("p");
+
+    phone.textContent = "(11) 3425-4293";
+    email.textContent = "parrilla@raul.com";
+    address.textContent = "Av. San Juan 4359, Ciudad Autónoma de Buenos Aires";
+
+    contact.appendChild(phone);
+    contact.appendChild(email);
+    contact.appendChild(address);
+
+    return contact;
 })();
 
 const contactTab = {tabHeading, tabContent};
